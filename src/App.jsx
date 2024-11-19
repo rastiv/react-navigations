91a64d0338d945a8b84bc1057e4990981fab2f73
Raycho Ivanov <rastiv.bg@gmail.com>
2024-11-19 14:28:53 +0200

    add AnimatedTabs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import Sidebar from "./components/sidebar/Sidebar";
 import ShiftingMenu from "./components/shiftingmenu/ShiftingMenu";
+import AnimatedTabs from "./components/animatedtabs/AnimatedTabs";
 
 function App() {
   const [open, setOpen] = useState(true);
@@ -10,7 +11,10 @@ function App() {
       <Sidebar open={open} setOpen={setOpen} />
       <motion.div layout className="h-[200vh] w-full">
         <ShiftingMenu />
-        <div className="p-8">OTHER CONTENT</div>
+
+        <div className="w-full flex justify-center py-10">
+          <AnimatedTabs />
+        </div>
       </motion.div>
     </div>
   );
---
+++ src/components/animatedtabs/AnimatedTabs.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+const tabs = [
+  { id: "home", label: "Home" },
+  { id: "about-us", label: "About Us" },
+  { id: "services", label: "Services" },
+  { id: "contact", label: "Contact" },
+  { id: "blog", label: "Blog" },
+];
+
+const AnimatedTabs = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+  return (
+    <motion.div layout className="flex space-x-1">
+      {tabs.map((tab) => (
+        <button
+          key={tab.id}
+          onClick={() => setActiveTab(tab.id)}
+          className={`${
+            activeTab === tab.id ? "" : "hover:text-slate-600"
+          } relative rounded-full px-3 py-1.5 text-slate-500 text-sm outline-2 font-medium transition`}
+        >
+          {activeTab === tab.id && (
+            <motion.div
+              layoutId="active-tab"
+              className="absolute inset-0 border border-indigo-300"
+              style={{ borderRadius: 9999 }}
+            />
+          )}
+
+          <span className="relative z-20">{tab.label}</span>
+        </button>
+      ))}
+    </motion.div>
+  );
+};
+
+export default AnimatedTabs;
